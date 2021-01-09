6cab11a47bc5e28a28b590895d616112e9adc138
Valentino Stoll <thevalentino+code@gmail.com>
2021-01-09 15:21:30 -0500

    ✅ Move database tests into its own file

--- test/slimer_test.rb
+++ test/slimer_test.rb
@@ -60,31 +60,4 @@ class SlimerTest < Minitest::Test
     database = Slimer.db
     assert_instance_of Sequel::SQLite::Database, database
   end
-
-  def test_db_with_env
-    require "sequel/core"
-
-    test_db_url = "sqlite://./tmp/slimer_test.db"
-    ENV["SLIMER_DATABASE_URL"] = test_db_url
-
-    database = Slimer.db
-    assert_equal test_db_url, Slimer.options[:database_url]
-    assert_instance_of Sequel::SQLite::Database, database
-
-    test_db_url = "sqlite://./tmp/slimer_test2.db"
-    ENV["SLIMER_DATABASE_URL"] = nil
-    ENV["DATABASE_URL"] = test_db_url
-
-    database = Slimer.db
-    assert_equal test_db_url, Slimer.options[:database_url]
-    assert_instance_of Sequel::SQLite::Database, database
-  end
-
-  def test_db_connection_auto_migrates
-    database = Slimer.db
-
-    Slimer::Database::REQUIRED_TABLES.each do |t|
-      assert database.table_exists?(t)
-    end
-  end
 end
