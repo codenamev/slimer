2c4db2c0351930c0cd804bb7b3106dd33308788c
Valentino Stoll <vstoll@doximity.com>
2021-01-25 09:04:11 -0500

    Move examples into their own repositories and link to them via submodules

--- examples/heroku/application.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "pg"
-require "slimer"
-
-Slimer.logger = Logger.new("#{__dir__}/slimer.log")
-Slimer.db
--- examples/heroku/config.ru
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative "application"
-require "slimer/web"
-
-run Slimer::Web
